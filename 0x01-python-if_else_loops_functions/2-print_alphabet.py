f4ee537cef193ae440b6a1b3112de1baf8adbb79
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2023-11-29 00:28:49 +0100

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-output = ""
-for char in range(ord('a'), ord('z') + 1):
-    if chr(char) not in ['q', 'e']:
-        output += "{}".format(chr(char))
-print(output)
+for i in range(97, 123):
+    print("{:c}".format(i), end="")
