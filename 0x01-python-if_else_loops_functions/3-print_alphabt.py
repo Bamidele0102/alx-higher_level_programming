02aa689d305dc47d390dabab94ec7fc51b99930b
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2023-11-29 00:22:44 +0100

    When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
+output = ""
 for char in range(ord('a'), ord('z') + 1):
-    if char not in [ord('e'), ord('q')]:
-        print("{:c}".format(char), end="")
-print()
+    if chr(char) not in ['q', 'e']:
+        output += "{}".format(chr(char))
+print(output)
