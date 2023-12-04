32f0e03ed5436a0df92d6c214d7e18d8c6c8d3eb
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2023-12-05 00:22:38 +0100

    Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    # Filter out characters 'c' and 'C'
-    filtered_string = ''.join(char for char in my_string if char.lower() not in {'c'})
-    return filtered_string
+    new_string = ""
+    for elements in my_string:
+        if elements != "c" and elements != "C":
+            new_string += elements
+    return new_string
