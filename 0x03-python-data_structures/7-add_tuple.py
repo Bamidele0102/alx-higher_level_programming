aacc37847e284bb15812165c0c37601d8087dc3b
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2023-12-05 00:29:53 +0100

    Tuples addition

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    # If tuples are smaller than 2, pad with 0
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
+    # Return a new tuple with the sums
+    return (a[0] + b[0], a[1] + b[1])
