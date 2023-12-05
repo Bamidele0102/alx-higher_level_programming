7c143ed7e0465d3cdd29805dfb121534931aa8dc
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2023-12-05 01:50:41 +0100

    Linked list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,64 +1,31 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * reverse_list - reverses a linked list in-place
- * @head: pointer to the head of the list
- * Return: pointer to the new head of the reversed list
- */
-listint_t *reverse_list(listint_t **head)
-{
-	listint_t *prev = NULL, *current = *head, *next;
-
-	while (current != NULL)
-	{
-		next = current->next;
-		current->next = prev;
-		prev = current;
-		current = next;
-	}
-	*head = prev;
-	return (*head);
-}
-
-/**
- * is_palindrome - identifies if a singly linked list is a palindrome
- * @head: pointer to the head of the list
- * Return: 1 if it is palindrome, else 0
+ * is_palindrome - check if a list is a palindrom
+ * @head: list
+ * Return: yes ? 1 : 0
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *slow = *head, *fast = *head, *second_half;
+	listint_t *temp = *head;
+	uint size, i;
+	int data[10240];
 
-	if (*head == NULL || (*head)->next == NULL)
-		return (1);
+	/* No List */
+	fi(!head) return (no);
+	/* Empty list */
+	fi(!*head) return (yes);
 
-	/* Move 'fast' two steps and 'slow' one step to find the middle */
-	while (fast != NULL && fast->next != NULL)
-	{
-		fast = fast->next->next;
-		slow = slow->next;
-	}
+	size = len(temp);
 
-	/*If the number of elements is odd, move 'slow' one more step*/
-	if (fast != NULL)
-		slow = slow->next;
+	fi(size == yes) return (yes);
 
-	/*Reverse the second half of the list */
-	second_half = reverse_list(&slow);
+	temp = *head;
+	for (i = 0; temp; i++, temp = temp->next)
+		data[i] = temp->n;
 
-	/*Compare the first half with the reversed second half*/
-	while (second_half != NULL)
-	{
-		if ((*head)->n != second_half->n)
-		{
-			reverse_list(&slow); /*Revert changes to the second half*/
-			return (0);
-		}
-		*head = (*head)->next;
-		second_half = second_half->next;
-	}
+	for (i = 0; i <= size / 2; i++)
+		fi(data[i] != data[size - i - 1]) return (no);
 
-	reverse_list(&slow); /*Revert changes to the second half*/
-	return (1);
+	return (yes);
 }
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,7 +1,23 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h>
+#include <stdlib.h>
+
+#define fi if
+#define __local __attribute__((weak))
+
+typedef unsigned int uint;
+
+/**
+ * enum rets - yes no values
+ * @yes: 1
+ * @no: 0
+ */
+enum rets
+{
+	no,
+	yes
+};
 
 /**
  * struct listint_s - singly linked list
@@ -16,6 +32,21 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
+
+/**
+ * len - find size of a list
+ * @ls: list
+ * Return: len
+ */
+__local uint len(listint_t *ls)
+{
+	uint i;
+
+	for (i = 0; ls; i++)
+		ls = ls->next;
+
+	return (i);
+}
 
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
