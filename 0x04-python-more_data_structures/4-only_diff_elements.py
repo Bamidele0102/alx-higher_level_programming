dc19304c4f77584936c6928226ed82837059d0e1
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2023-12-06 22:44:13 +0100

    Only differents

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    # Use the symmetric_difference operator ^ to find elements unique to each set
     diff_set = set_1 ^ set_2
     return diff_set
