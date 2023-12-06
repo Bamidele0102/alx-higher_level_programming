0e6953d1c26da3f3058875a27e39fbeb58b296ce
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2023-12-06 22:20:05 +0100

    Search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def search_replace(my_list, search, replace):
     # Create a new list with replaced elements
     new_list = [replace if num == search else num for num in my_list]
