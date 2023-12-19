ae45b1e1db42eda210676cb1c8e79bc24dda6485
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2023-12-20 00:33:10 +0100

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This is a simple Square class with a private instance attribute.
+"""
+
+class Square:
+    """
+    The Square class represents a geometric square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a new instance of the Square class.
+
+        Parameters:
+        - size (int): The size of the square.
+
+        Note:
+        The size parameter is not type or value verified.
+        """
+        self.__size = size
