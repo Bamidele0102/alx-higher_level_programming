11bdd066f9e6b75ac4e783eee2b958cf6c743ee8
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2023-12-20 00:29:12 +0100

    My first square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This is a simple script with an empty Square class.
+"""
+
+
+class Square:
+    """
+    The Square class represents a geometric square.
+    """
+    pass
