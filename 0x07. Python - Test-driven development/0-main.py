1024805826130d47e87f9e01603f516586fe01e8
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-01-06 14:45:57 +0100

    Integers addition

--- 0x07. Python - Test-driven development/0-main.py
+++ 0x07. Python - Test-driven development/0-main.py
@@ -1,15 +1,31 @@
 #!/usr/bin/python3
-add_integer = __import__('0-add_integer').add_integer
+def add_integer(a, b=98):
+    """
+    Adds two integers.
 
-print(add_integer(1, 2))
-print(add_integer(100, -2))
-print(add_integer(2))
-print(add_integer(100.3, -2))
-try:
-    print(add_integer(4, "School"))
-except Exception as e:
-    print(e)
-try:
-    print(add_integer(None))
-except Exception as e:
-    print(e)
+    Args:
+        a (int or float): The first parameter.
+        b (int or float): The second parameter. Default is 98.
+
+    Returns:
+        int: The sum of a and b.
+
+    Raises:
+        TypeError: If a or b is not an integer or float.
+
+    Examples:
+        >>> add_integer(1, 2)
+        3
+        >>> add_integer(100, -2)
+        98
+        >>> add_integer(2)
+        100
+        >>> add_integer(100.3, -2)
+        98
+    """
+    if not (isinstance(a, int) or isinstance(a, float)):
+        raise TypeError("a must be an integer or b must be an integer")
+    if not (isinstance(b, int) or isinstance(b, float)):
+        raise TypeError("a must be an integer or b must be an integer")
+
+    return int(a) + int(b)
