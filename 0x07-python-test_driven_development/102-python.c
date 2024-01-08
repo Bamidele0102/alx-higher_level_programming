ea71df4c2ca4b9eba676034aa9cf32672f09755d
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-01-08 07:58:54 +0100

    CPython #3: Python Strings

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,4 +1,4 @@
-#include "python3.10/Python.h"
+#include "Python.h"
 
 /**
  * print_python_string - Prints information about Python strings.
@@ -6,44 +6,23 @@
  */
 void print_python_string(PyObject *p)
 {
-	Py_ssize_t length;
+	long int length;
 
 	fflush(stdout);
+
 	printf("[.] string object info\n");
-	if (!PyUnicode_Check(p))
+	if (strcmp(p->ob_type->tp_name, "str") != 0)
 	{
 		printf("  [ERROR] Invalid String Object\n");
 		return;
 	}
-	length = PyUnicode_GET_LENGTH(p);
+
+	length = ((PyASCIIObject *)(p))->length;
 
 	if (PyUnicode_IS_COMPACT_ASCII(p))
 		printf("  type: compact ascii\n");
 	else
 		printf("  type: compact unicode object\n");
 	printf("  length: %ld\n", length);
-
-	if (PyUnicode_IS_COMPACT_ASCII(p))
-	{
-		const char *value = PyUnicode_AsUTF8(p);
-
-		if (!value)
-		{
-			PyErr_Print();
-			return;
-		}
-		printf("  value: %s\n", value);
-	}
-	else
-	{
-		wchar_t *value = PyUnicode_AsWideCharString(p, &length);
-
-		if (!value)
-		{
-			PyErr_Print();
-			return;
-		}
-		printf("  value: %ls\n", value);
-		PyMem_Free(value);
-	}
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
 }
