ed6e5593d2e1bcce9433c8d2b510f9844011311d
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-01-09 16:40:10 +0100

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,12 +3,6 @@
 
 
 def read_file(filename=""):
-    """Reads a file and prints its content."""
-    try:
-        with open(filename, 'r', encoding='utf-8') as file:
-            content = file.read()
-            print(content)
-    except FileNotFoundError:
-        print(f"File '{filename}' not found.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
