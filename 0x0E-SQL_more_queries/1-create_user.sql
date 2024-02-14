ef75733fd1277acd71f142e720b371e90c21c625
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-02-14 22:17:02 +0100

    Root user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,7 @@
+-- Creates the MySQL server user user_0d_1 with all privileges
+-- Password should be set to user_0d_1_pwd and 
+-- Should not fail if user already exists
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
