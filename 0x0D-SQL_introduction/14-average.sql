e339fae62e0233bb7c0978aa46c1c5964d067892
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-02-14 03:31:08 +0100

    Average

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,6 @@
+-- Computes the score average of all record in the table
+-- Result column name should be 'average'
+-- Database name will be passed as an argument of the mysql command
+
+SELECT AVG(score) AS 'average'
+FROM second_table;
